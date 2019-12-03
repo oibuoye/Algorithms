1d131e4e770b4272715659896844a0dc7a5294f0
oibuoye <ope4sure1@gmail.com>
2019-12-03 12:11:55 +0100

    Added a solution to algorithm for checking the largest matrix using dynamic programming.

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Algorithms.Q;
+using Algorithms.VanHack;
 using Hangfire;
 using Hangfire.SqlServer;
 using Microsoft.Owin.Hosting;
@@ -15,9 +16,18 @@ namespace Algorithms
     {
         static void Main(string[] args)
         {
-
-            //int result = MakeArrayConsecutive_MissingNumbers_.MissingNumbers(inputArray);
-
+            var list1 = new List<int>() { 1, 0, 1, 1, 1 };
+            var list2 = new List<int>() { 1, 0, 1, 1, 0 };
+            var list3 = new List<int>() { 1, 1, 1, 1, 0 };
+            var list4 = new List<int>() { 1, 1, 1, 1, 0 };
+            var list5 = new List<int>() { 1, 1, 1, 1, 1 };
+            var numbers = new List<List<int>>();
+            numbers.Add(list1);
+            numbers.Add(list2);
+            numbers.Add(list3);
+            numbers.Add(list4);
+            numbers.Add(list5);
+            int result = ImageEditing.largestMatrix(numbers);
 
             Console.ReadLine();
         }
---
+++ Algorithms/VanHack/ImageEditing.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.VanHack
+{
+    public class ImageEditing
+    {
+        public static int largestMatrix(List<List<int>> arr)
+        {
+            //var list1 = new List<int>() { 1, 0, 1, 1, 1 };
+            //var list2 = new List<int>() { 1, 0, 1, 1, 0 };
+            //var list3 = new List<int>() { 1, 1, 1, 1, 0 };
+            //var list4 = new List<int>() { 1, 1, 1, 1, 0 };
+            //var list5 = new List<int>() { 1, 1, 1, 1, 1 };
+            //var numbers = new List<List<int>>();
+            //numbers.Add(list1);
+            //numbers.Add(list2);
+            //numbers.Add(list3);
+            //numbers.Add(list4);
+            //numbers.Add(list5);
+            //int result = ImageEditing.largestMatrix(numbers);
+
+
+
+
+            //Get the length of the list
+            int arrLength = arr.Count;
+            int largestWhiteSpace = 0;
+
+            //Create temp matrix
+            int[,] tempMatrix = new int[arrLength, arrLength];
+
+            //Fill the first row of the temp matrix with the value from first list of the int list
+            List<int> firstRow = arr[0];
+            for(int i =0; i < arrLength; i++)
+            {
+                tempMatrix[0, i] = firstRow[i];
+            }
+
+            //Fill the column of the temp matrix with the value from first column of the int list
+            for (int i = 0; i < arrLength; i++)
+            {
+                List<int> arrList = arr[i];
+                tempMatrix[i, 0] = arrList[0];
+            }
+
+            //Fill the rest part of the temp matrix
+            for (int i = 1; i < arrLength; i++)
+            {
+                List<int> previousRow = arr[i -1];
+                List<int> currentRow = arr[i];
+
+                for (int j = 1; j < arrLength; j++)
+                {
+                    if (currentRow[j] == 1)
+                    {
+                        //Check the minimum value of the diagonal cell, the cell up and the cell by the left
+                        int currentCellValue = Math.Min(previousRow[j - 1], Math.Min(previousRow[j], currentRow[j - 1])) + 1;
+                        //save it into the tempMatrix
+                        tempMatrix[i, j] = currentCellValue;
+                        //save it to the current list
+                        currentRow[j] = currentCellValue;
+
+                        if(largestWhiteSpace <= currentCellValue)
+                        {
+                            largestWhiteSpace = currentCellValue;
+                        }
+                    }
+                    else
+                    {
+                        tempMatrix[i, j] = 0;
+                    }
+                }
+            }
+
+            return largestWhiteSpace;
+        }
+    }
+}
